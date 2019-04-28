7d95865ba4d87a85499dced73fb8c3f679b1b480
P.B.Vignesh <plumbum_7@yahoo.com>
2019-04-28 15:53:47 +0530

    Adding BoilerPlate code for Insert and Select Commands

--- clonelite.c
+++ clonelite.c
@@ -5,10 +5,27 @@
 
 enum META_COMMAND_RESULT_T {
     META_COMMAND_SUCCESS,
-    META_COMMAND_INVALID
+    META_COMMAND_FAILURE_INVALID
+};
+
+enum PREPARE_COMMAND_RESULT_T {
+    PREPARE_SUCCESS,
+    PREPARE_FAILURE_INVALID
+};
+
+enum STATEMENT_TYPE_T {
+    STATEMENT_SELECT,
+    STATEMENT_INSERT
 };
 
 typedef enum META_COMMAND_RESULT_T META_COMMAND_RESULT;
+typedef enum PREPARE_COMMAND_RESULT_T PREPARE_COMMAND_RESULT;
+typedef enum STATEMENT_TYPE_T STATEMENT_TYPE;
+typedef struct Statement_t Statement;
+
+struct Statement_t {
+    STATEMENT_TYPE type;
+};
 
 /**
  * Function to print the Clone Lite Prompt
@@ -63,7 +80,51 @@ META_COMMAND_RESULT execute_meta_command(char * buffer)
         exit(EXIT_SUCCESS);
     }
 
-    return META_COMMAND_INVALID;
+    return META_COMMAND_FAILURE_INVALID;
+}
+
+/**
+ * Function to prepare the statement for execution
+ *
+ * @param buffer The buffer containing the command
+ * @param statement The final prepared statement output
+ *
+ * @return PREPARE_COMMAND_RESULT The status of preparing the command
+ */
+PREPARE_COMMAND_RESULT prepare_statement(char * buffer, Statement * statement)
+{
+    if (strncmp(buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+
+        return PREPARE_SUCCESS;
+    }
+
+    if (strncmp(buffer, "select", 6) == 0) {
+        statement->type = STATEMENT_SELECT;
+
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_FAILURE_INVALID;
+}
+
+/**
+ * Function to execute the non meta commands
+ *
+ * @param statement The prepared statement
+ *
+ * @return void
+ */
+void execute_statement(Statement * statement)
+{
+    switch (statement->type) {
+        case STATEMENT_INSERT:
+            // TODO: Handle Insertion Logic
+            break;
+        case STATEMENT_SELECT:
+            // TODO: Handle Selection Logic
+            break;
+    }
 }
 
 /**
@@ -84,13 +145,24 @@ int main(int argc, char * argv[])
         if (is_meta_command(buffer)) {
             switch (execute_meta_command(buffer)) {
                 case META_COMMAND_SUCCESS:
-                    break;
-                case META_COMMAND_INVALID:
+                    continue;
+                case META_COMMAND_FAILURE_INVALID:
                     printf("[+] Invalid Command '%s'.\n", buffer);
-                    break;
+                    continue;
             }
         } else {
-            printf("[+] Invalid Command '%s'.\n", buffer);
+            Statement statement;
+            switch (prepare_statement(buffer, &statement)) {
+                case PREPARE_SUCCESS:
+                    break;
+                case PREPARE_FAILURE_INVALID:
+                    printf("[+] Invalid Keyword at start of '%s'\n", buffer);
+                    continue;
+            }
+
+            // No need to check if this ran correctly because it will run correctly for sure
+            execute_statement(&statement);
+            printf("[+] Statement Executed.\n");
         }
     }
 
